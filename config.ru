74774f92a7f5c32a6e8716df75f4844101d604f2
Travis Graham <tgraham@smithandhammaker.com>
2010-01-11 20:48:08 -0500

    Initial commit

---
+++ config.ru
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sinatra'
+require 'net/https'
+require 'haml'
+
+require 'iyd'
+
+run Sinatra::Application
---
+++ iyd.rb
@@ -0,0 +1,12 @@
+http = Net::HTTP.new('www.yammer.com', 443)
+req = Net::HTTP::Get.new('/login')
+http.use_ssl = true
+response = http.request(req)
+
+get '/' do
+  if response.code.to_i == 200
+    haml :go
+  else
+    haml :awry
+  end
+end
